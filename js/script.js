739c1ff24649fc7d1cdd3d917befc4d52a5d607f
Andrianovi <106331579+Andrianovi@users.noreply.github.com>
2023-01-13 20:27:56 +0500

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$('.burger').click(function (event) {
-		$('.burger_svg-wrapper,.burger,.burger-menu').toggleClass('active');
-		$('body').toggleClass('locker');
-	});
-});
-
-
-
